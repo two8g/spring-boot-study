d14885a13c5c9e63365cdd0f3539ad9f2e2b189d
two8g <395120985@qq.com>
2017-12-07 12:13:57 +0800

    add starter-web dep, add hello request handler

--- helloword/src/main/java/demo/helloword/HelloWordApplication.java
+++ helloword/src/main/java/demo/helloword/HelloWordApplication.java
@@ -2,11 +2,20 @@ package demo.helloword;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWordApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWordApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWordApplication.class, args);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
+    public String hello() {
+        return "Hello";
+    }
 }
